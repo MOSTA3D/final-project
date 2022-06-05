c736bfe7981ccdc99c590db05f6abaa0e0d68e6a
MOSTA3D <khabbieny123@gmail.com>
2022-06-05 14:02:51 +0200

    fixing the SERVER_URL problem

--- finalProjectApp/src/utils/config.js
+++ finalProjectApp/src/utils/config.js
@@ -1,4 +1,4 @@
 // change server url in deployment
 
 // export const SERVER_URL = "https://detectorserver.azurewebsites.net";
-export const SERVER_URL = "http://localhost:3001";
+export const SERVER_URL = window.location.host === "http://localhost:3000" ? "http://localhost:3001" : "https://detectorserver.azurewebsites.net";
